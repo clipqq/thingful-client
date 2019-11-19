4b025f0188b799d08cb2729d6044f65f70374d29
Clifford Tran <clipqq>
2019-11-19 12:56:07 -0800

    added jwt auth

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -6,6 +6,7 @@ import {
   Button,
   Input
 } from '../Utils/Utils'
+import AuthApiService from '../../services/auth-api-service'
 
 export default class LoginForm extends Component {
   static defaultProps = {
@@ -13,6 +14,8 @@ export default class LoginForm extends Component {
       // DO SOMETHING
     }
   }
+
+  
 
   state = {
     error: null
@@ -35,11 +38,29 @@ export default class LoginForm extends Component {
       TokenService.makeBasicAuthToken(user_name.value, password.value)
     )
 
-
-
     user_name.value = ''
     password.value = ''
     this.props.onLoginSuccess()
+  }
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    this.setState({ error: null })
+    const { user_name, password } = ev.target
+ 
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then(res => {
+        user_name.value = ''
+        password.value = ''
+        TokenService.saveAuthToken(res.authToken)
+        this.props.onLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
   }
 
   render() {
@@ -47,9 +68,7 @@ export default class LoginForm extends Component {
       error
     } = this.state
     return ( <form className = 'LoginForm'
-      onSubmit = {
-        this.handleSubmitBasicAuth
-      } >
+    onSubmit={this.handleSubmitJwtAuth} >
       <div role = 'alert' > {
         error && < p className = 'red' > {
           error
