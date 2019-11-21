684842faacb9e4ce4582980a5a43d6d139e808f9
Clifford Tran <clipqq>
2019-11-20 20:13:19 -0800

    added timeout expiry

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,4 +1,5 @@
 import config from '../config'
+import TokenService from '../services/token-service'
 
 const AuthApiService = {
     postLogin(credentials) {
@@ -29,6 +30,25 @@ const AuthApiService = {
                 res.json()
             )
     },
+    postRefresh() {
+        const authToken = TokenService.getAuthToken()
+        return fetch(`${config.API_ENDPOINT}/auth/refresh`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `bearer ${authToken}`
+                }
+            })
+            .then(res =>
+                (!res.ok) ?
+                res.json().then(e => Promise.reject(e)) :
+                res.json()
+            )
+            .then(data => {
+                console.log(`refresh data: ${data}`)
+                TokenService.saveAuthToken(data.authToken)
+            })
+    }
 }
 
 export default AuthApiService
--- src/services/token-service.js
+++ src/services/token-service.js
@@ -1,14 +1,26 @@
 import config from '../config'
+import AuthApiService from '../services/auth-api-service'
+
+let _refreshTimeoutId;
 
 const TokenService = {
   saveAuthToken(token) {
-    window.localStorage.setItem(config.TOKEN_KEY, token)
+    window.sessionStorage.setItem(config.TOKEN_KEY, token)
+    _refreshTimeoutId = setTimeout(function () {
+      AuthApiService.postRefresh()
+    }, 15 * 1000)
   },
   getAuthToken() {
-    return window.localStorage.getItem(config.TOKEN_KEY)
+    return window.sessionStorage.getItem(config.TOKEN_KEY)
   },
   clearAuthToken() {
-    window.localStorage.removeItem(config.TOKEN_KEY)
+    window.sessionStorage.removeItem(config.TOKEN_KEY)
+    setTimeout(function () {
+      /* remove the refresh timeout from the queue */
+      clearTimeout(_refreshTimeoutId)
+      /* logout */
+      TokenService.clearToken()
+    }, 50 * 1000)
   },
   hasAuthToken() {
     return !!TokenService.getAuthToken()
